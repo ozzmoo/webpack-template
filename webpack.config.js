0bb174f5301ffebe77fd6816713287eb52f90ef3
ozzmo <mestervalera@gmail.com>
2019-03-20 00:31:32 +0500

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge')
+const pug = require('./webpack/pug')
+const devserver = require('./webpack/devserver')
 
 const PATHS = {
   source: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build')
 };
 
-const common = {
-  entry: PATHS.source + '/index.js',
-  output: {
-    path: PATHS.build,
-    filename: '[name].js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: PATHS.source + '/index.pug'
-    })
-  ],
-  module:{
-    rules:[
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: true
-        }
-      }
+const common = merge ([
+  {
+    entry: PATHS.source + '/index.js',
+    output: {
+      path: PATHS.build,
+      filename: '[name].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: PATHS.source + '/index.pug'
+      })
     ]
-  }
-};
+  },
+  pug()
+  
+]);
 
-const developmentConfig = {
-  devServer: {
-    stats: 'errors-only',
-    port: 9000
-  }
-};
 
 module.exports = function(env){
   if (env === 'production'){
     return common;
   }
   if (env === 'development'){
-    return Object.assign(
-      {},
+    return merge([
       common,
-      developmentConfig
-    )
+      devserver()
+    ])
   }
 }
 
